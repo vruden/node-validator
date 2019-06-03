82df9f6f80ce987e7575cbc34174fae1ea792f2e
Vladimir Rudenko <ruden.v.a@gmail.com>
2019-06-03 17:31:06 +0600

    Added strict option to required validator

--- test/required.js
+++ test/required.js
@@ -7,15 +7,24 @@ const asyncValidate = require('../lib/async-validator').validate;
 describe('required', function () {
     describe('should set default values', function () {
         const data = {
-            first_name: 'Tom'
+            first_name: 'Tom',
+            age: null,
+            second_name: null,
+            range1: [],
+            range2: [],
+            string1: '   ',
+            string2: '   '
         };
 
         const result = {
             age : [
                 'Возраст cannot be blank.'
             ],
-            second_name: [
-                'Second name cannot be blank.'
+            range1: [
+                'Range1 cannot be blank.'
+            ],
+            string1: [
+                'String1 cannot be blank.'
             ],
             parents: [
                 'Field parents cannot be blank.'
@@ -23,20 +32,21 @@ describe('required', function () {
         };
 
         const rules = [
-            [['age', 'first_name', 'second_name'], 'required'],
+            [['age', 'first_name', 'range1', 'string1'], 'required'],
+            [['second_name', 'range2', 'string2'], 'required', {strict: true}],
             [['parents'], 'required', {message: 'Field {attribute} cannot be blank.', lowercaseLabel: true}]
         ];
 
         it('sync', async function () {
-            validate(rules, data, {age: 'Возраст'});
+            const errors = validate(rules, data, {age: 'Возраст'});
 
-            assert.deepEqual(result, result);
+            assert.deepEqual(result, errors);
         });
 
         it('async', async function () {
-            await asyncValidate(rules, data, {age: 'Возраст'});
+            const errors = await asyncValidate(rules, data, {age: 'Возраст'});
 
-            assert.deepEqual(result, result);
+            assert.deepEqual(result, errors);
         });
     });
 });
